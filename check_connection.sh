b0288e31b874192f54d6f908bd803057ae940685
Saut Daniel Panjaitan <daniel.panjaitan@satunol.com>
2016-08-11 14:39:49 +0700

    update ip location

--- check_connection.sh
+++ check_connection.sh
@@ -1,17 +1,17 @@
 ((count = 10))                            # Maximum number to try.
-while [[ $count -ne 0 ]] ; do
-#while [[ 1 ]] ; do
-    ping -I eth0 -c 1 8.8.8.8                      # Try once.
+#while [[ $count -ne 0 ]] ; do
+while [[ 1 ]] ; do
+    ping -I eth1 -c 1 8.8.8.8                      # Try once.
     rc=$?
     if [[ $rc -eq 0 ]] ; then
     (
 			echo "ping is succesfull"
-			curl --data "ethernet=1" http://192.168.10.105/post.php
+			curl --data "ethernet=1" http://192.168.10.104/post.php
 		)
 		else
 		(
 			echo "time out"
-			curl --data "ethernet=0" http://192.168.10.105/post.php
+			curl --data "ethernet=0" http://192.168.10.104/post.php
 		)
     fi
     ((count = count - 1))                  # So we don't go forever.
