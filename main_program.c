1fc17e8ade498fcf7e83ffe7d16227f593d211aa
Saut Daniel Panjaitan <daniel.panjaitan@satunol.com>
2016-09-07 15:02:29 +0700

    adding kwhtrap and th discovery

--- main_program.c
+++ main_program.c
@@ -1,9 +1,13 @@
 #include "mac_address.c"
 #include <stdio.h>
+#include "res_sensor.c"
 
+char location[] = "http://192.168.10.117/post_trap.php";
 int main()
 	{
-	uint16_t gateway_ID = mac_address();
-	printf("Gateway id is %d", gateway_ID);
+		uint16_t gateway_ID = mac_address();
+		//trap_kwh(location, 10, gateway_ID, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19);
+		trap_th_discovery(location, 10, gateway_ID, 1245, 1111);
+	//printf("Gateway id is %d", gateway_ID);
 	return 0;
 	}
--- res_sensor.c
+++ res_sensor.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <curl/curl.h>
+#include <string.h>
+char temp[128];
+char scoreData[300];
 int  res_kwh (char* location,uint16_t VoltR,uint16_t VoltS,uint16_t VoltT,uint16_t CurrentR,
 								uint16_t CurrentS,uint16_t CurrentT, int nilai,int device)
 
 {
-   char scoreData[128];
    snprintf(scoreData, sizeof scoreData, "location=%s",location);
 	{
 		snprintf(scoreData, sizeof scoreData, "VoltR=%d&VoltS=%d&VoltT=%d&CurrentR=%d&CurrentS=%d&CurrentT=%d&device=%d&nilai=%d",
@@ -27,28 +29,65 @@ int  res_kwh (char* location,uint16_t VoltR,uint16_t VoltS,uint16_t VoltT,uint16
 	}
 	return 0;
 }
+
+int trap_th_discovery (char* location, int uID, uint16_t gateway_ID, uint16_t th_ID, int sensorUsed)
+{
+	// uID th keberapa yang konek ke si gateway
+	// thID dan Gateway ID sudah jelas
+	// sensorUsed id 4 bit, 1111 (kelembapan, suhu, suhu, suhu) 1 nyala 0 mati
+	snprintf(scoreData, sizeof scoreData, "trapVariable=18,0,3,0:%d;18,0,54,0:%d;18,%d,39,0:%d",
+					gateway_ID, th_ID, uID, sensorUsed); 
+	CURL *curl;
+	CURLcode res; 
+	curl_global_init(CURL_GLOBAL_ALL);
+	curl = curl_easy_init();
+	if(curl) { 
+    curl_easy_setopt(curl, CURLOPT_URL, location);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, scoreData);
+    res = curl_easy_perform(curl);
+    if(res != CURLE_OK)
+	    fprintf(stderr, "curl_easy_perform() failed: %s\n",curl_easy_strerror(res));
+	    curl_easy_cleanup(curl);
+	 	}   
+	  curl_global_cleanup();
+	return 0;
+}
 int trap_kwh (char* location, int channel, uint16_t id,uint16_t VoltR,uint16_t VoltS,uint16_t VoltT
 						,uint16_t CurrentR,uint16_t CurrentS,uint16_t CurrentT,uint16_t FrequencyR,uint16_t FrequencyS,uint16_t FrequencyT
 						,uint16_t PowerFactorR,uint16_t PowerFactorS,uint16_t PowerFactorT,uint16_t WattR,uint16_t WattS,uint16_t WattT
 						,uint16_t KwhPrdR,uint16_t KwhPrdS,uint16_t KwhPrdT,uint16_t TotalKwhPrd)
 
 {
-   char scoreData[128];
-   snprintf(scoreData, sizeof scoreData, "location=%s",location);
-	{
 		int channelTrap = channel + 1;
-		snprintf(scoreData, sizeof scoreData, "trapVariable=21,0,3,0:%d;21,%d,5,0:%d;21,%d,6,0:%d;21,%d,7,0:%d
-						;21,%d,8,0:%d;21,%d,9,0:%d;21,%d,10,0:%d;21,%d,11,0:%d;21,%d,12,0:%d
-						;21,%d,13,0:%d;21,%d,14,0:%d;21,%d,15,0:%d;21,%d,16,0:%d;21,%d,17,0:%d
-						;21,%d,18,0:%d;21,%d,19,0:%d;21,%d,27,0:%d;21,%d,28,0:%d;21,%d,29,0:%d;21,%d,30,0:%d",
-						id, channel, VoltR, channel, VoltT, channel, VoltS, channel
-						, channel, CurrentR, channel, CurrentT, channel, CurrentS, channel
-						, channel, FrequencyR, channel, FrequencyT, channel, FrequencyS, channel
-						, channel, PowerFactorR, channel, PowerFactorT, channel, PowerFactorS, channel
-						, channel, WattR, channel, WattT, channel, WattS, channel
-						, channel, KwhPrdR, channel, KwhPrdT, channel, KwhPrdS, channel
-						, channel, TotalKwhPrd
-						);
+		/*snprintf(scoreData, sizeof scoreData, "",
+						, channel
+						, channel
+						, channel
+						,  channel
+						, 
+						);*/
+		snprintf(scoreData, sizeof scoreData, "trapVariable=21,0,3,0:%d;21,%d,5,0:%d;21,%d,6,0:%d;21,%d,7,0:%d",
+					id, channelTrap, VoltR, channelTrap, VoltS, channelTrap, VoltT); 
+		snprintf(temp, sizeof scoreData,  ";21,%d,8,0:%d;21,%d,9,0:%d;21,%d,10,0:%d"
+						, channelTrap, CurrentR, channelTrap, CurrentS, channelTrap, CurrentT);
+		strcat(scoreData, temp);
+		printf("scoreData %s\n", scoreData);	
+		snprintf(temp, sizeof scoreData,  ";21,%d,11,0:%d;21,%d,12,0:%d;21,%d,13,0:%d"
+						, channelTrap, FrequencyR, channelTrap, FrequencyS, channelTrap, FrequencyT);
+		strcat(scoreData, temp);
+		printf("scoreData %s\n", scoreData);	
+		snprintf(temp, sizeof scoreData,  ";21,%d,14,0:%d;21,%d,15,0:%d;21,%d,16,0:%d"
+						, channelTrap, PowerFactorR, channelTrap, PowerFactorS, channelTrap, PowerFactorT);
+		strcat(scoreData, temp);
+		printf("scoreData %s\n", scoreData);	
+		snprintf(temp, sizeof scoreData,  ";21,%d,17,0:%d;21,%d,18,0:%d;21,%d,19,0:%d"
+						, channelTrap, WattR, channelTrap, WattS, channelTrap, WattT);
+		strcat(scoreData, temp);
+		printf("scoreData %s\n", scoreData);	
+		snprintf(temp, sizeof scoreData,  ";21,%d,27,0:%d;21,%d,28,0:%d;21,%d,29,0:%d;21,%d,30,0:%d"
+						,channelTrap, KwhPrdR, channelTrap, KwhPrdS, channelTrap, KwhPrdT, channelTrap, TotalKwhPrd);
+		strcat(scoreData, temp);
+		printf("scoreData %s\n", scoreData);	
 		  CURL *curl;
 		  CURLcode res; 
 		  curl_global_init(CURL_GLOBAL_ALL);
@@ -64,13 +103,12 @@ int trap_kwh (char* location, int channel, uint16_t id,uint16_t VoltR,uint16_t V
 		  	}   
 		  curl_global_cleanup();
 		
-	}
+	
 	return 0;
 }
 int  res_th (char* location,uint16_t Th1,uint16_t Th2,uint16_t Th3,uint16_t Humidity, int nilai,int device)
 
 {
-   char scoreData[128];
    snprintf(scoreData, sizeof scoreData, "location=%s",location);
 	{
 		snprintf(scoreData, sizeof scoreData, "Th1=%d&Th2=%d&Th3=%d&Humidity=%d&device=%d&nilai=%d",
