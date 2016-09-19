f92e78865fc01116005b7f5ed7984ea5dae85dd0
Mihawk76 <sdanielchp@gmail.com>
2016-09-19 09:45:27 +0700

    adding amazon server

--- main_program.c
+++ main_program.c
@@ -2,12 +2,19 @@
 #include <stdio.h>
 #include "res_sensor.c"
 
-char location[] = "http://192.168.10.117/post_trap.php";
+char location[] = "http://192.168.88.19:1616/dcms/rest/alfa";
 int main()
 	{
 		uint16_t gateway_ID = mac_address();
-		//trap_kwh(location, 10, gateway_ID, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19);
-		trap_th(location, 10, gateway_ID, 1245,1,2,1356,3005,5,6,7);
-	//printf("Gateway id is %d", gateway_ID);
-	return 0;
+		int humidity = 3004;
+		int temp1 = 2005;
+		int temp2 = 3005;
+		int temp3 = 3005;
+		int rssi = -10;
+		//trap_kwh(location, 10, gateway_ID, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19)
+		//trap_th(location, 10, gateway_ID, 1245,1,2,1356,3005,5,6,7);
+		//res_th (location, 3003, 3005, 34000, 4000, 11, gateway_ID);
+		trap_th (location, 1, gateway_ID, 0x1003, 0, 0, humidity, temp1, temp2, temp3, rssi);
+		//printf("Gateway id is %d", gateway_ID);
+		return 0;
 	}
--- res_sensor.c
+++ res_sensor.c
@@ -36,7 +36,7 @@ int trap_th_discovery (char* location, int uID, uint16_t gateway_ID, uint16_t th
 	// thID dan Gateway ID sudah jelas
 	// sensorUsed id 4 bit, 1111 (kelembapan, suhu, suhu, suhu) 1 nyala 0 mati
 	// btw trap_th_connect sama dengan trap_th_discovery
-	snprintf(scoreData, sizeof scoreData, "trapVariable=18,0,3,0:%d;18,0,54,0:%d;18,%d,39,0:%d",
+	snprintf(scoreData, sizeof scoreData, "trapVariable=18.0.3.0:%d;18.0.54.0:%d;18.%d.39.0:%d",
 					gateway_ID, th_ID, uID, sensorUsed); 
 	CURL *curl;
 	CURLcode res; 
@@ -87,9 +87,9 @@ int trap_th (char* location, int uID, uint16_t gateway_ID, uint16_t th_ID, uint1
 	// sensorUsed id 4 bit, 1111 (kelembapan, suhu, suhu, suhu) 1 nyala 0 mati
 	// btw trap_th_connect sama dengan trap_th_discovery
 	printf("rssi %d\n", rssi);
-	snprintf(scoreData, sizeof scoreData, "trapVariable=18,0,3,0:%d;18,%d,1,0:%d;18,%d,10,0:%d;18,%d,12,0:%d",
+	snprintf(scoreData, sizeof scoreData, "18.0.3.0:%d;18.%d.1.0:%d;18.%d.10.0:%d;18.%d.12.0:%d",
 					gateway_ID, uID, th_ID, uID, dIn1, uID, dIn2 ); 
-	snprintf(temp, sizeof scoreData,  ";18,%d,40,0:%d%d;18,%d,41,0:%d;18,%d,42,0:%d;18,%d,43,0:%d"
+	snprintf(temp, sizeof scoreData,  ";18.%d.40.0:%d%d;18.%d.41.0:%d;18.%d.42.0:%d;18.%d.43.0:%d"
 					, uID, hummidity, th1, uID, th2, uID, th3, uID, rssi);
 	strcat(scoreData, temp);
 	printf("scoreData %s\n", scoreData);	
@@ -122,25 +122,25 @@ int trap_kwh (char* location, int channel, uint16_t id,uint16_t VoltR,uint16_t V
 						,  channel
 						, 
 						);*/
-		snprintf(scoreData, sizeof scoreData, "trapVariable=21,0,3,0:%d;21,%d,5,0:%d;21,%d,6,0:%d;21,%d,7,0:%d",
+		snprintf(scoreData, sizeof scoreData, "trapVariable=21.0.3.0:%d;21.%d.5.0:%d;21.%d.6.0:%d;21.%d.7.0:%d",
 					id, channelTrap, VoltR, channelTrap, VoltS, channelTrap, VoltT); 
-		snprintf(temp, sizeof scoreData,  ";21,%d,8,0:%d;21,%d,9,0:%d;21,%d,10,0:%d"
+		snprintf(temp, sizeof scoreData,  ";21.%d.8.0:%d;21.%d.9.0:%d;21.%d.10.0:%d"
 						, channelTrap, CurrentR, channelTrap, CurrentS, channelTrap, CurrentT);
 		strcat(scoreData, temp);
 		printf("scoreData %s\n", scoreData);	
-		snprintf(temp, sizeof scoreData,  ";21,%d,11,0:%d;21,%d,12,0:%d;21,%d,13,0:%d"
+		snprintf(temp, sizeof scoreData,  ";21.%d.11.0:%d;21.%d.12.0:%d;21.%d.13.0:%d"
 						, channelTrap, FrequencyR, channelTrap, FrequencyS, channelTrap, FrequencyT);
 		strcat(scoreData, temp);
 		printf("scoreData %s\n", scoreData);	
-		snprintf(temp, sizeof scoreData,  ";21,%d,14,0:%d;21,%d,15,0:%d;21,%d,16,0:%d"
+		snprintf(temp, sizeof scoreData,  ";21.%d.14.0:%d;21.%d.15.0:%d;21.%d.16.0:%d"
 						, channelTrap, PowerFactorR, channelTrap, PowerFactorS, channelTrap, PowerFactorT);
 		strcat(scoreData, temp);
 		printf("scoreData %s\n", scoreData);	
-		snprintf(temp, sizeof scoreData,  ";21,%d,17,0:%d;21,%d,18,0:%d;21,%d,19,0:%d"
+		snprintf(temp, sizeof scoreData,  ";21.%d.17.0:%d;21.%d.18.0:%d;21.%d.19.0:%d"
 						, channelTrap, WattR, channelTrap, WattS, channelTrap, WattT);
 		strcat(scoreData, temp);
 		printf("scoreData %s\n", scoreData);	
-		snprintf(temp, sizeof scoreData,  ";21,%d,27,0:%d;21,%d,28,0:%d;21,%d,29,0:%d;21,%d,30,0:%d"
+		snprintf(temp, sizeof scoreData,  ";21.%d.27.0:%d;21.%d.28.0:%d;21.%d.29.0:%d;21.%d.30.0:%d"
 						,channelTrap, KwhPrdR, channelTrap, KwhPrdS, channelTrap, KwhPrdT, channelTrap, TotalKwhPrd);
 		strcat(scoreData, temp);
 		printf("scoreData %s\n", scoreData);	
