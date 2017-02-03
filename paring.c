4b97353225cf2ba99a2fd6cbcc5c06d751ea1a13
Daniel <sdanielchp@gmail.com>
2017-02-03 16:29:25 +0700

    working code

--- paring.c
+++ paring.c
@@ -3,17 +3,82 @@
 #include <mysql.h>
 #include <string.h>
 #include <stdlib.h>
+#include "paring.h"
 
 //char*id_getway[100];
 int total_pairing;
-int pairing_id[100];
+int pairing_id[1000];
 int mysql_id;
+typedef struct{
+	int suhu;
+	int no_byte;
+	int value_byte;
+}IR_COMMAND;
+
+IR_COMMAND ir_command[1000];
 
 void finish_with_error(MYSQL *con)
 {
   fprintf(stderr, "%s\n", mysql_error(con));
   mysql_close(con);
   exit(1);        
+}
+void get_ir_command(char* server, char* user ,char* password ,char* dbname,char* nm_table,char* tipe_ac)
+{      
+  MYSQL *con = mysql_init(NULL);
+
+  if (con == NULL) 
+  {
+      fprintf(stderr, "mysql_init() failed\n");
+      exit(1);
+  }  
+  
+  if (mysql_real_connect(con, server, user, password, 
+          dbname, 0, NULL, 0) == NULL) 
+  {
+      finish_with_error(con);
+  }    
+	char select[100];
+ sprintf(select,"select suhu, no_byte, value_byte from  %s where tipe_ac='%s'",nm_table,tipe_ac); 
+ if (mysql_query(con,select)) 
+ { 
+      finish_with_error(con);
+ }
+
+
+ MYSQL_RES *result = mysql_store_result(con);  
+ if (result == NULL) 
+ {
+      finish_with_error(con);
+ }
+
+	int num_fields = mysql_num_fields(result);
+	MYSQL_ROW row;
+	int i;
+	int a=0;
+	{
+   while((row = mysql_fetch_row(result)) != NULL)
+    {
+     /*for (i = 0; i < num_fields; i++)
+        {
+      total_pairing = a+i;     
+			pairing_id[total_pairing]= atoi(row[i]?row[i]:"NULL");
+			printf("%d %d\n", pairing_id[total_pairing], total_pairing);
+        //printf("data_library:%02X \n",pairing_id[total_pairing]);
+        }
+		a=a+i;*/
+			ir_command[a].suhu = atoi(row[0]?row[0]:"NULL");	
+			ir_command[a].no_byte = atoi(row[1]?row[1]:"NULL");
+			ir_command[a].value_byte = atoi(row[2]?row[2]:"NULL");
+			printf("%d %d %02X\n", ir_command[a].suhu, ir_command[a].no_byte, ir_command[a].value_byte);
+			a++;
+  		//printf("\n"); 
+			//if (ir_command[a].suhu == 30){
+				//break;
+			//}
+    }
+	}
+
 }
 void get_id_pairing(char* server, char* user ,char* password ,char* dbname,char* nm_table,int kk)
 {      
@@ -31,7 +96,7 @@ void get_id_pairing(char* server, char* user ,char* password ,char* dbname,char*
       finish_with_error(con);
   }    
 	char select[100];
- sprintf(select,"select id_paring from  %s where id_getway=%d",nm_table,kk); 
+ sprintf(select,"select id_paring from  %s where tipe_ac=%d",nm_table,kk); 
  if (mysql_query(con,select)) 
  { 
       finish_with_error(con);
---
+++ paring.h
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#ifndef paring_h_
+#define paring_h_
+
+
+void get_ir_command(char* server, char* user ,char* password ,char* dbname,char* nm_table,char* tipe_ac);
+
+#endif // paring_h_
